770fac9d6be7bd6437605b7a0c8e500d317b888a
Farhan899 <farhanbhai2u@gmail.com>
2025-12-29 16:54:38 +0500

    yzz

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -3,9 +3,9 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     DATABASE_URL: str
     BETTER_AUTH_SECRET: str
-    FRONTEND_URL: str = "http://localhost:3000/"
+    FRONTEND_URL: str = "https://todoapp-2-farhan.netlify.app"
     ENVIRONMENT: str = "development"
-    # API_HOST: str = "0.0.0.0"
+    API_HOST: str = "0.0.0.0"
     API_PORT: int = 8000
     LOG_LEVEL: str = "INFO"
 
